55c1141592b789e987bfcbe153ca3f3ddfc4c4f8
mirva <mirvan2599@gmail.com>
2023-06-10 14:55:51 +0700

    Create get movie detail movie unit test

--- app/src/test/java/com/mirvan/moviedb/AppFeatureUnitTest.kt
+++ app/src/test/java/com/mirvan/moviedb/AppFeatureUnitTest.kt
@@ -1,10 +1,12 @@
 package com.mirvan.moviedb
 
 import com.mirvan.moviedb.App_feature.data.remote.MainApi
+import com.mirvan.moviedb.App_feature.data.repositoryImpl.DetailMovieRepositoryImpl
 import com.mirvan.moviedb.App_feature.data.repositoryImpl.GetGenresRepositoryImpl
 import com.mirvan.moviedb.App_feature.data.repositoryImpl.MovieByGenresRepositoryImpl
 import com.mirvan.moviedb.App_feature.domain.model.Genres
 import com.mirvan.moviedb.App_feature.domain.model.MovieByGenre
+import com.mirvan.moviedb.App_feature.domain.model.MovieDetail
 import com.mirvan.moviedb.Core.util.Resource
 import com.mirvan.moviedb.utils.enqueueResponse
 import kotlinx.coroutines.flow.collectLatest
@@ -29,6 +31,7 @@ class AppFeatureUnitTest {
         .create(MainApi::class.java)
     private val getGenresRepository = GetGenresRepositoryImpl(api)
     private val movieByGenresRepository = MovieByGenresRepositoryImpl(api)
+    private val detailMovieRepository = DetailMovieRepositoryImpl(api)
 
     @After
     fun tearDown() {
@@ -617,5 +620,177 @@ class AppFeatureUnitTest {
         }
     }
 
+    @Test
+    fun `should fetch get detail movie correctly given 200 response`() {
+        mockWebServer.enqueueResponse("get-detail-movie-response-200.json", 200)
 
+        runBlocking {
+            detailMovieRepository.getDetailMovie(603692, "en").collectLatest { result ->
+                val expected = MovieDetail(
+                    adult = false,
+                    belongs_to_collection = MovieDetail.BelongsToCollection(
+                        id = 404609,
+                        name = "John Wick Collection",
+                        poster_path = "/xUidyvYFsbbuExifLkslpcd8SMc.jpg",
+                        backdrop_path = "/fSwYa5q2xRkBoOOjueLpkLf3N1m.jpg"
+                    ),
+                    budget = 90000000,
+                    genres = listOf(
+                        MovieDetail.Genre(
+                            id = 28,
+                            name = "Action"
+                        ),
+                        MovieDetail.Genre(
+                            id = 53,
+                            name = "Thriller"
+                        ),
+                        MovieDetail.Genre(
+                            id = 80,
+                            name = "Crime"
+                        )
+                    ),
+                    homepage = "https://johnwick.movie",
+                    id = 603692,
+                    imdb_id = "tt10366206",
+                    original_language = "en",
+                    original_title = "John Wick: Chapter 4",
+                    overview = "With the price on his head ever increasing, John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.",
+                    popularity = 3701.122,
+                    poster_path = "/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+                    production_companies = listOf(
+                        MovieDetail.ProductionCompany(
+                            id = 3528,
+                            logo_path = "/cCzCClIzIh81Fa79hpW5nXoUsHK.png",
+                            name = "Thunder Road",
+                            origin_country = "US"
+                        ),
+                        MovieDetail.ProductionCompany(
+                            id = 23008,
+                            logo_path = "/5SarYupipdiejsEqUkwu1SpYfru.png",
+                            name = "87Eleven",
+                            origin_country = "US"
+                        ),
+                        MovieDetail.ProductionCompany(
+                            id = 491,
+                            logo_path = "/5LvDUt3KmvRnXQ4NrdWJYHeuA8J.png",
+                            name = "Summit Entertainment",
+                            origin_country = "US"
+                        ),
+                        MovieDetail.ProductionCompany(
+                            id = 264,
+                            logo_path = "/fA90qwUKgPhMONqtwY60GaHRyrW.png",
+                            name = "Studio Babelsberg",
+                            origin_country = "DE"
+                        )
+                    ),
+                    production_countries = listOf(
+                        MovieDetail.ProductionCountry(
+                            iso_3166_1 = "DE",
+                            name = "Germany"
+                        ),
+                        MovieDetail.ProductionCountry(
+                            iso_3166_1 = "US",
+                            name = "United States of America"
+                        )
+                    ),
+                    release_date = "2023-03-22",
+                    revenue = 431769198,
+                    runtime = 170,
+                    spoken_languages = listOf(
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Arabic",
+                            iso_639_1 = "ar",
+                            name = "العربية"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Cantonese",
+                            iso_639_1 = "cn",
+                            name = "广州话 / 廣州話"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "English",
+                            iso_639_1 = "en",
+                            name = "English"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "French",
+                            iso_639_1 = "fr",
+                            name = "Français"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "German",
+                            iso_639_1 = "de",
+                            name = "Deutsch"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Japanese",
+                            iso_639_1 = "ja",
+                            name = "日本語"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Latin",
+                            iso_639_1 = "la",
+                            name = "Latin"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Russian",
+                            iso_639_1 = "ru",
+                            name = "Pусский"
+                        ),
+                        MovieDetail.SpokenLanguage(
+                            english_name = "Spanish",
+                            iso_639_1 = "es",
+                            name = "Español"
+                        )
+                    ),
+                    status = "Released",
+                    tagline = "No way back, one way out.",
+                    title = "John Wick: Chapter 4",
+                    video = false,
+                    vote_average = 7.949,
+                    vote_count = 2792,
+                    backdrop_path = "/h8gHn0OzBoaefsYseUByqsmEDMY.jpg"
+                )
+                when (result) {
+                    is Resource.Success -> {
+                        val actual: MovieDetail = result.data!!
+                        assertEquals(expected.genres?.size, actual.genres?.size)
+                        for (i in 0 until expected.genres?.size!!) {
+                            val expectedGenre = expected.genres!![i]
+                            val actualGenre = actual.genres!![i]
+
+                            assertEquals(expectedGenre.id, actualGenre.id)
+                            assertEquals(
+                                expectedGenre.name,
+                                actualGenre.name
+                            )
+                        }
+                    }
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `shouldn't fetch detail movies by genre given 401 response`() {
+        mockWebServer.enqueueResponse("error-response-401.json", 401)
+
+        runBlocking {
+            detailMovieRepository.getDetailMovie(
+                603692,
+                "en"
+            ).collectLatest { result ->
+                val expected = "Invalid API key: You must be granted a valid key."
+                when (result) {
+                    is Resource.Error -> {
+                        val actual: String? = result.message
+                        assertEquals(expected, actual)
+                    }
+                    else -> {
+                    }
+                }
+            }
+        }
+    }
 }
